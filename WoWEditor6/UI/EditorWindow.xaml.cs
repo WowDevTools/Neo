8918bbf32582b18addc48bc6be2eb83aa5baf485
Cromon <mugadr.m@gmail.com>
2015-02-16 14:29:22 +0100

    Started implementing the UI completely with WPF.

--- WoWEditor6/UI/EditorWindow.xaml.cs
+++ WoWEditor6/UI/EditorWindow.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
+using Microsoft.Win32;
 using WoWEditor6.Scene;
+using WoWEditor6.Win32;
 
 namespace WoWEditor6.UI
 {
@@ -72,6 +77,23 @@ namespace WoWEditor6.UI
                 pr.Inlines.Add(titleRun);
                 pr.Inlines.Add(new Run(message));
                 LogDocument.Blocks.Add(pr);
+                var scroller = LogDocument.Parent as FlowDocumentScrollViewer;
+                if (scroller == null)
+                    return;
+
+                if (VisualTreeHelper.GetChildrenCount(scroller) == 0)
+                    return;
+
+                var child = VisualTreeHelper.GetChild(scroller, 0);
+                if (child == null)
+                    return;
+
+                var border = VisualTreeHelper.GetChild(child, 0) as Decorator;
+                if (border == null)
+                    return;
+
+                var scrollView = border.Child as ScrollViewer;
+                if(scrollView != null) scrollView.ScrollToBottom();
             }));
         }
 
@@ -80,5 +102,240 @@ namespace WoWEditor6.UI
             Log.RemoveSink(this);
         }
 
+        private void BrowseFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fd = (IFileOpenDialog)
+                Activator.CreateInstance(Type.GetTypeFromCLSID(new Guid("{DC1C5A9C-E88A-4DDE-A5A1-60F82A20AEF7}")));
+
+            Fos options;
+            fd.GetOptions(out options);
+            options |= Fos.FosPickfolders;
+            fd.SetOptions(options);
+
+
+            var wnd = InterfaceManager.Instance.RenderWindow;
+            var result = 0;
+            if (wnd.InvokeRequired)
+                wnd.Invoke(new Action(() => result = fd.Show(wnd.Handle)));
+            else
+                result = fd.Show(IntPtr.Zero);
+
+            if (result != 0)
+                return;
+
+            IShellItem item;
+            fd.GetResult(out item);
+            if (item == null)
+                return;
+
+            var ptrOut = IntPtr.Zero;
+            try
+            {
+                item.GetDisplayName(Sigdn.Filesyspath, out ptrOut);
+                PathTextBox.Text = Marshal.PtrToStringUni(ptrOut);
+            }
+            catch (Exception)
+            {
+                item.GetDisplayName(Sigdn.Normaldisplay, out ptrOut);
+                PathTextBox.Text = Marshal.PtrToStringUni(ptrOut);
+            }
+            finally
+            {
+                if (ptrOut != IntPtr.Zero)
+                    Marshal.FreeCoTaskMem(ptrOut);
+            }
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PathTextBox.Text))
+                return;
+
+            SplashDocument.Visibility = Visibility.Collapsed;
+            LoadingDocument.Visibility = Visibility.Visible;
+
+            IO.FileManager.Instance.DataPath = PathTextBox.Text;
+            IO.FileManager.Instance.LoadComplete += () =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    LoadingDocument.Visibility = Visibility.Collapsed;
+                    InitMaps();
+                });
+            };
+
+            IO.FileManager.Instance.InitFromPath();
+        }
+
+        private void InitMaps()
+        {
+            MapTileGrid.Children.Clear();
+
+            var filter = MapFilterTextBox.Text;
+
+            var children = new List<StackPanel>();
+            for (var i = 0; i < Storage.DbcStorage.Map.NumRows; ++i)
+            {
+                var row = Storage.DbcStorage.Map.GetRow(i);
+                var title = row.GetString(Storage.MapFormatGuess.FieldMapTitle);
+                if (string.IsNullOrEmpty(filter) == false && title.ToLowerInvariant().Contains(filter.ToLowerInvariant()) == false)
+                    continue;
+
+                var panel = new StackPanel
+                {
+                    Orientation = Orientation.Vertical,
+                    Width = 120,
+                    Height = 120,
+                    Background = new SolidColorBrush(Color.FromRgb(80, 80, 80)),
+                    Margin = new Thickness(5,5,0,0)
+                };
+
+                var titleLabel = new TextBlock
+                {
+                    Text = title,
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    FontSize = 16,
+                    Foreground = System.Windows.Media.Brushes.White,
+                    TextWrapping = TextWrapping.Wrap,
+                    Margin = new Thickness(3,3,3,3)
+                };
+
+                panel.Children.Add(titleLabel);
+                panel.Tag = false;
+
+                panel.MouseEnter += (sender, args) =>
+                {
+                    var down = panel.Tag as Boolean?;
+                    if (down ?? true)
+                        return;
+
+                    panel.Background = new SolidColorBrush(Color.FromRgb(120, 120, 120));
+                    
+                };
+
+                panel.MouseLeave += (sender, args) =>
+                {
+                    var down = panel.Tag as Boolean?;
+                    if (down ?? true)
+                        return;
+                    panel.Background = new SolidColorBrush(Color.FromRgb(80, 80, 80));
+                };
+
+                panel.MouseDown += (sender, args) =>
+                {
+                    panel.Background = new SolidColorBrush(Color.FromRgb(60, 60, 60));
+                    panel.Tag = true;
+                    panel.CaptureMouse();
+                };
+
+                panel.MouseUp += (sender, args) =>
+                {
+                    if ((panel.Tag as Boolean?) ?? false)
+                    {
+                        EntrySelectView.MapSelected(row.GetInt32(0));
+                        var scroll = MapTileGrid.Parent as ScrollViewer;
+                        if (scroll == null)
+                            return;
+
+                        var g = scroll.Parent as Grid;
+                        if (g != null)
+                            g.Visibility = Visibility.Collapsed;
+
+                        EntrySelectView.Visibility = Visibility.Visible;
+                    }
+
+                    panel.Background =
+                        new SolidColorBrush(panel.IsMouseOver ? Color.FromRgb(120, 120, 120) : Color.FromRgb(80, 80, 80));
+                    panel.Tag = false;
+                    panel.ReleaseMouseCapture();
+                };
+
+                children.Add(panel);
+            }
+
+            if (MapSortTypeCheckBox.IsChecked ?? false)
+                children.Sort((e1, e2) => String.Compare(((TextBlock) e1.Children[0]).Text, ((TextBlock) e2.Children[0]).Text, StringComparison.Ordinal));
+
+            children.ForEach(c => MapTileGrid.Children.Add(c));
+
+            var scroller = MapTileGrid.Parent as ScrollViewer;
+            if (scroller == null)
+                return;
+
+            var grid = scroller.Parent as Grid;
+            if (grid != null)
+                grid.Visibility = Visibility.Visible;
+        }
+
+        private void RegistryButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = LoadFromKey(Registry.CurrentUser) ?? LoadFromKey(Registry.LocalMachine);
+
+            if (result == null)
+                return;
+
+            PathTextBox.Text = result;
+        }
+
+        private static string LoadFromKey(RegistryKey baseKey)
+        {
+            var rootKey = IntPtr.Size == 8
+                ? "Software\\WoW6432Node\\Blizzard Entertainment\\World of Warcraft"
+                : "Software\\Blizzard Entertainment\\World of Warcraft";
+
+            try
+            {
+                var wowKey = baseKey.OpenSubKey(rootKey);
+                if (wowKey == null)
+                    return null;
+                return wowKey.GetValue("InstallPath") as string;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private void MapTileGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var element = (sender as Grid);
+            if (element == null)
+                return;
+
+            var parent = element.Parent as Grid;
+            if (parent == null)
+                return;
+
+            element.Height = parent.ActualHeight;
+        }
+
+        private void WelcomePanel_Resize(object sender, SizeChangedEventArgs e)
+        {
+            var element = sender as Grid;
+            if (element == null)
+                return;
+
+            var scroller = MapTileGrid.Parent as ScrollViewer;
+            if (scroller == null)
+                return;
+
+            var grid = scroller.Parent as Grid;
+            if (grid == null)
+                return;
+
+            grid.Height = element.ActualHeight;
+            grid.RowDefinitions[1].Height = new GridLength(element.ActualHeight - grid.RowDefinitions[0].Height.Value);
+        }
+
+        private void FilterText_Changed(object sender, TextChangedEventArgs e)
+        {
+            InitMaps();
+        }
+
+        private void MapSortCheck_Click(object sender, RoutedEventArgs e)
+        {
+            InitMaps();
+        }
     }
 }
---
+++ WoWEditor6/UI/WpfImageSource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using WoWEditor6.IO.Files.Texture;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+
+namespace WoWEditor6.UI
+{
+    static class WpfImageSource
+    {
+        private static readonly BitmapSource ErrorBitmap;
+
+        public unsafe static BitmapSource FromBgra(int width, int height, uint[] colors)
+        {
+            fixed (uint* ptr = colors)
+                return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, new IntPtr(ptr), colors.Length * 4, width * 4);
+        }
+
+        public static BitmapSource FromBgra(int width, int height, byte[] colors)
+        {
+            return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, colors, width * 4);
+        }
+
+        public static BitmapSource FromTexture(string fileName)
+        {
+            var texLoadInfo = TextureLoader.LoadToArgbImage(fileName);
+            return texLoadInfo == null ? ErrorBitmap : FromBgra(texLoadInfo.Width, texLoadInfo.Height, texLoadInfo.Layers[0]);
+        }
+
+        static WpfImageSource()
+        {
+            var bmp = new Bitmap(200, 200, PixelFormat.Format32bppArgb);
+            var g = System.Drawing.Graphics.FromImage(bmp);
+            var font = new System.Drawing.Font("Segoe UI", 26);
+            g.Clear(System.Drawing.Color.Black);
+            var len = g.MeasureString("Invalid image", font);
+            g.DrawString("Invalid Image", font, System.Drawing.Brushes.Red, new PointF(100 - len.Width / 2.0f, 100 - len.Height / 2.0f));
+            font.Dispose();
+            g.Flush();
+            g.Dispose();
+
+            var bmpd = bmp.LockBits(new Rectangle(0, 0, 200, 200), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            ErrorBitmap = BitmapSource.Create(200, 200, 96, 96, PixelFormats.Bgra32, null, bmpd.Scan0, 200*200*4, 200*4);
+            bmp.UnlockBits(bmpd);
+            bmp.Dispose();
+        }
+    }
+}
