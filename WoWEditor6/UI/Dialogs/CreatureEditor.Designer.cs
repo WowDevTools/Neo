80a3160914c4d6e4185f371c94caadea7c40e636
Kaev <Demonhunter@hotmail.de>
2015-02-19 20:13:15 +0100

    Placed the CreatureEditorControl on the Creature Editor-Form. Don't know why i didn't pushed that before.

--- WoWEditor6/UI/Dialogs/CreatureEditor.Designer.cs
+++ WoWEditor6/UI/Dialogs/CreatureEditor.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.creatureEditorControl1 = new WoWEditor6.UI.Dialogs.CreatureEditorControl();
+            this.creatureEditorControl1.TabControl.SuspendLayout();
+            this.creatureEditorControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // creatureEditorControl1
+            // 
+            this.creatureEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.creatureEditorControl1.Location = new System.Drawing.Point(0, 0);
+            this.creatureEditorControl1.Name = "creatureEditorControl1";
+            this.creatureEditorControl1.Size = new System.Drawing.Size(1251, 558);
+            // 
+            // creatureEditorControl1.TabControl
+            // 
+            this.creatureEditorControl1.TabControl.Location = new System.Drawing.Point(587, 29);
+            this.creatureEditorControl1.TabControl.Name = "TabControl";
+            this.creatureEditorControl1.TabControl.SelectedIndex = 0;
+            this.creatureEditorControl1.TabControl.Size = new System.Drawing.Size(663, 399);
+            this.creatureEditorControl1.TabControl.TabIndex = 108;
+            this.creatureEditorControl1.TabIndex = 0;
             // 
             // CreatureEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 482);
+            this.ClientSize = new System.Drawing.Size(1251, 558);
+            this.Controls.Add(this.creatureEditorControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "CreatureEditor";
             this.Text = "Creature Editor";
+            this.creatureEditorControl1.TabControl.ResumeLayout(false);
+            this.creatureEditorControl1.ResumeLayout(false);
+            this.creatureEditorControl1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private CreatureEditorControl creatureEditorControl1;
     }
 }
