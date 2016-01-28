9a8acebe647d9308566a77ad0fce940738c86bcc
Mathias <mathais234@gmail.com>
2016-01-28 19:24:43 +0100

    Started work on tablet support
    Currently Texture and Sculpting is working

--- WoWEditor6/Editing/EditManager.cs
+++ WoWEditor6/Editing/EditManager.cs
@@ -2,6 +2,8 @@
 using SharpDX;
 using WoWEditor6.Scene;
 using WoWEditor6.UI;
+using WintabDN;
+using System.Windows;
 
 namespace WoWEditor6.Editing
 {
@@ -13,6 +15,11 @@ namespace WoWEditor6.Editing
 
         private float mInnerRadius = 45.0f;
         private float mOuterRadius = 55.0f;
+
+        // Stuff for the pen system 
+        // can be cleaned up
+        private CWintabContext m_logContext = null;
+        private CWintabData m_wtData = null;
 
         public float InnerRadius
         {
@@ -36,6 +43,11 @@ namespace WoWEditor6.Editing
         static EditManager()
         {
             Instance = new EditManager();
+        }
+
+        EditManager()
+        {
+            InitDataCapture();
         }
 
         public void UpdateChanges()
@@ -90,6 +102,107 @@ namespace WoWEditor6.Editing
             WorldFrame.Instance.UpdateBrush(mInnerRadius, mOuterRadius);
             if (EditorWindowController.Instance.TexturingModel != null)
                 EditorWindowController.Instance.TexturingModel.HandleOuterRadiusChanged(value);
+        }
+
+        private void InitDataCapture()
+        {
+            try
+            {
+                CloseCurrentContext();
+
+                m_logContext = OpenQueryDigitizerContext();
+
+                m_wtData = new CWintabData(m_logContext);
+                m_wtData.SetWTPacketEventHandler(HandlePenMessage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private CWintabContext OpenQueryDigitizerContext()
+        {
+            bool status = false;
+            CWintabContext logContext = null;
+
+            try
+            {
+                // Get the default digitizing context.
+                // Default is to receive data events.
+                logContext = CWintabInfo.GetDefaultDigitizingContext(ECTXOptionValues.CXO_MESSAGES);
+
+                // Set system cursor if caller wants it.
+                logContext.Options |= (uint)ECTXOptionValues.CXO_SYSTEM;
+
+                if (logContext == null)
+                {
+                    return null;
+                }
+
+                // Modify the digitizing region.
+                logContext.Name = "WintabDN Event Data Context";
+
+                // output in a 10000 x 10000 grid
+                logContext.OutOrgX = logContext.OutOrgY = 0;
+                logContext.OutExtX = 10000;
+                logContext.OutExtY = 10000;
+
+
+                // Open the context, which will also tell Wintab to send data packets.
+                status = logContext.Open();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return logContext;
+        }
+
+        public void HandlePenMessage(Object sender_I, MessageReceivedEventArgs eventArgs_I)
+        {
+            if (m_wtData == null)
+            {
+                return;
+            }
+
+            try
+            {
+                uint pktID = (uint)eventArgs_I.Message.WParam;
+                WintabPacket pkt = m_wtData.GetDataPacket(pktID);
+
+                if (pkt.pkContext != 0)
+                {
+                    // Normalize data between 0 and 40
+                    float normalizedValue = (40f - 0f) / ((float)CWintabInfo.GetMaxPressure() - 0f) * ((float)pkt.pkNormalPressure.pkAbsoluteNormalPressure - (float)CWintabInfo.GetMaxPressure()) + 40f;
+                    TextureChangeManager.Instance.Amount = normalizedValue;
+                    TerrainChangeManager.Instance.Amount = normalizedValue;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void CloseCurrentContext()
+        {
+            try
+            {
+                //TraceMsg("Closing context...\n");
+                if (m_logContext != null)
+                {
+                    m_logContext.Close();
+                    m_logContext = null;
+                    m_wtData = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
