661819e457a1cc1ab009a2ba323cd41834e38bc1
westtunger <westtunger@gmail.com>
2016-08-04 18:45:40 +0200

    Removed unused import.

--- WoWEditor6/Storage/Database/WotLk/TrinityCore/IItem.cs.cs
+++ WoWEditor6/Storage/Database/WotLk/TrinityCore/IItem.cs.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WoWEditor6.Storage.Database.WotLk.TrinityCore
+﻿namespace WoWEditor6.Storage.Database.WotLk.TrinityCore
 {
     public interface IItem
     {
--- WoWEditor6/Storage/Database/WotLk/TrinityCore/ItemManager.cs
+++ WoWEditor6/Storage/Database/WotLk/TrinityCore/ItemManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Data;
-using System.Windows.Forms;
 
 namespace WoWEditor6.Storage.Database.WotLk.TrinityCore
 {
