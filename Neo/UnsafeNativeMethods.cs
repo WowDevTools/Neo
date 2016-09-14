7daaf50a9823f9b6d039c9ca7ad42b5ffd52cd1e
Jarl Gullberg <jarl.gullberg@gmail.com>
2016-09-14 22:30:11 +0200

    Moved merged file and deleted unused P/Invoke functions.

--- Neo/UnsafeNativeMethods.cs
+++ Neo/UnsafeNativeMethods.cs
@@ -1,24 +1,14 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Security;
-using System.Text;
 
 namespace Neo
 {
     internal static unsafe class UnsafeNativeMethods
     {
-        [DllImport("Kernel32.dll", EntryPoint = "RtlMoveMemory", SetLastError = false)]
-        [SuppressUnmanagedCodeSecurity]
-        internal static extern IntPtr MoveMemory(byte* dest, byte* src, int count);
-
         [DllImport("Kernel32.dll", SetLastError = false)]
         [SuppressUnmanagedCodeSecurity]
         internal static extern IntPtr CopyMemory(byte* dest, byte* src, int count);
-
-        [DllImport("User32.dll", SetLastError = false)]
-        [SuppressUnmanagedCodeSecurity]
-        internal static extern int ToUnicode(int wVirtKey, int scanCode, byte[] keyState, StringBuilder outBuffer,
-            int numBUffer, int flags);
 
         [DllImport("User32.dll", SetLastError = false)]
         [SuppressUnmanagedCodeSecurity]
