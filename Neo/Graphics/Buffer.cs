be8f3990d9ae909a0f0346630f76f47abe05629d
Jarl Gullberg <jarl.gullberg@gmail.com>
2016-09-23 15:24:53 +0200

    Implemented data buffering. Calls to UpdateData have been marked obsolete and are now forwarded to BufferData.

--- Neo/Graphics/Buffer.cs
+++ Neo/Graphics/Buffer.cs
@@ -45,6 +45,15 @@ namespace Neo.Graphics
 		    private set;
 	    }
 
+	    /// <summary>
+		/// Creates a new instance of the <see cref="Buffer"/> class, generating a buffer object
+		/// on the GPU in the process.
+		/// </summary>
+		/// <param name="bufferType">The type of buffer to be created.</param>
+		/// <param name="bufferUsageHint">
+		/// The hinted usage of the buffer.
+		/// Note that this is not a binding promise, merely a hint to the underlying driver.
+		/// </param>
         protected Buffer(BufferTarget bufferType, BufferUsageHint bufferUsageHint)
         {
 	        this.glBufferID = GL.GenBuffer();
@@ -58,83 +67,61 @@ namespace Neo.Graphics
         }
 
 	    /// <summary>
+		/// Buffers a n array of data into graphics memory. If the buffer already contains a
+		/// set of data, the old data is deleted and replaced with the new data.
+		/// </summary>
+		/// <param name="data">An array of data values.</param>
+		/// <typeparam name="T">The type of the data to buffer.</typeparam>
+	    public void BufferData<T>(T[] data) where T : struct
+	    {
+		    if (data == null)
+		    {
+			    throw new ArgumentNullException(nameof(data), $"The \"{nameof(data)}\" argument was passed with a null value.");
+		    }
+
+		    if (data.Length <= 0)
+		    {
+			    throw new ArgumentException($"The {nameof(data)} array was empty.", nameof(data));
+		    }
+
+		    Bind();
+
+		    GL.BufferData(this.BufferType, (IntPtr)data.Length, data, this.BufferUsage);
+	    }
+
+	    /// <summary>
 	    /// Binds this buffer object, setting it as the current object to be operated on in the
-	    /// scope of the OpenGL pipeline.
+	    /// current thread context.
 	    /// </summary>
 	    public void Bind()
 	    {
 		    GL.BindBuffer(BufferType, this.glBufferID);
 	    }
 
-        private void Dispose(bool disposing)
-        {
-	        if (this.glBufferID > 0)
-	        {
-		        GL.DeleteBuffer(this.glBufferID);
-	        }
-        }
-
-        public virtual void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
 	    [Obsolete]
         public void UpdateData<T>(T data) where T : struct
         {
-            //Resize(IO.SizeCache<T>.Size, data);
+	        BufferData(new []{data});
         }
 
 	    [Obsolete]
         public void UpdateData<T>(T[] data) where T : struct
         {
-            //Resize(data.Length * IO.SizeCache<T>.Size, data);
+	        BufferData(data);
         }
 
-	    /*
-        private void Resize<T>(int length, T value) where T : struct
-        {
-            if (length > mDescription.SizeInBytes)
-            {
-                mDescription.SizeInBytes = length;
-                if (BufferID != null)
-                    BufferID.Dispose();
+	    private void Dispose(bool disposing)
+	    {
+		    if (this.glBufferID > 0)
+		    {
+			    GL.DeleteBuffer(this.glBufferID);
+		    }
+	    }
 
-                using (var strm = new DataStream(length, true, true))
-                {
-                    strm.Write(value);
-                    strm.Position = 0;
-                    BufferID = new SharpDX.Direct3D11.Buffer(mContext.Device, strm, mDescription);
-                }
-            }
-            else
-                mContext.Context.UpdateSubresource(ref value, BufferID);
-        }
-
-        private void Resize<T>(int length, T[] data) where T : struct
-        {
-            if (length > mDescription.SizeInBytes)
-            {
-                mDescription.SizeInBytes = length;
-                if (BufferID != null)
-                    BufferID.Dispose();
-
-                if (data != null)
-                {
-                    using (var strm = new DataStream(length, true, true))
-                    {
-                        strm.WriteRange(data);
-                        strm.Position = 0;
-                        BufferID = new SharpDX.Direct3D11.Buffer(mContext.Device, strm, mDescription);
-                    }
-                }
-                else
-                    BufferID = new SharpDX.Direct3D11.Buffer(mContext.Device, mDescription);
-            }
-            else
-                mContext.Context.UpdateSubresource(data, BufferID);
-        }
-		*/
+	    public virtual void Dispose()
+	    {
+		    Dispose(true);
+		    GC.SuppressFinalize(this);
+	    }
     }
 }
--- Neo/Graphics/VertexBuffer.cs
+++ Neo/Graphics/VertexBuffer.cs
@@ -2,8 +2,18 @@
 
 namespace Neo.Graphics
 {
+	/// <summary>
+	/// The <see cref="VertexBuffer"/> class is a specialized data container class for
+	/// vertices on the GPU.
+	///
+	/// When this object is disposed, the memory occupied by the data is marked as
+	/// released on the GPU.
+	/// </summary>
 	public class VertexBuffer : Buffer
     {
+	    /// <summary>
+		/// Initializes a new instanece of the <see cref="VertexBuffer"/> class.
+		/// </summary>
         public VertexBuffer() :
             base(BufferTarget.ArrayBuffer, BufferUsageHint.StaticDraw)
         {
