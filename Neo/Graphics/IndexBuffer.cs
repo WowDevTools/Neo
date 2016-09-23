e3dc04d36c7648cbb652052de068471c7d772b40
Jarl Gullberg <jarl.gullberg@gmail.com>
2016-09-23 15:29:34 +0200

    Fleshed out comments throughout the buffer classes.

--- Neo/Graphics/IndexBuffer.cs
+++ Neo/Graphics/IndexBuffer.cs
@@ -2,14 +2,28 @@
 
 namespace Neo.Graphics
 {
+	/// <summary>
+	/// The <see cref="VertexBuffer"/> class is a specialized data container class for
+	/// element indices on the GPU.
+	///
+	/// When this object is disposed, the memory occupied by the data is marked as
+	/// released on the GPU.
+	/// </summary>
 	public class IndexBuffer : Buffer
     {
+	    /// <summary>
+		/// The data type of the indices stored in this buffer.
+		/// </summary>
 	    public DrawElementsType IndexFormat { get; set; }
 
-	    public IndexBuffer() :
+	    /// <summary>
+		/// Initializes a new instance of the <see cref="IndexBuffer"/> class.
+		/// </summary>
+		/// <param name="indexFormat">The data type of the indices which will be stored in this buffer.</param>
+	    public IndexBuffer(DrawElementsType indexFormat) :
             base(BufferTarget.ElementArrayBuffer, BufferUsageHint.StaticDraw)
-        {
-	        IndexFormat = DrawElementsType.UnsignedInt;
-        }
+	    {
+		    IndexFormat = indexFormat;
+	    }
     }
 }
--- Neo/Graphics/UniformBuffer.cs
+++ Neo/Graphics/UniformBuffer.cs
@@ -2,8 +2,18 @@
 
 namespace Neo.Graphics
 {
+	/// <summary>
+	/// The <see cref="VertexBuffer"/> class is a specialized data container class for
+	/// uniform constant values which are passed to the shader programs on the GPU.
+	///
+	/// When this object is disposed, the memory occupied by the data is marked as
+	/// released on the GPU.
+	/// </summary>
 	public class UniformBuffer : Buffer
     {
+	    /// <summary>
+		/// Initializes a new instance of the <see cref="UniformBuffer"/> class.
+		/// </summary>
         public UniformBuffer()
             : base(BufferTarget.UniformBuffer, BufferUsageHint.StaticDraw)
         {
